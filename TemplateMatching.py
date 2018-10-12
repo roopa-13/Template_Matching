dcd9d86a0bad1bcf182027869f2296d7d26633d2
roopa-13 <43585230+roopa-13@users.noreply.github.com>
2018-10-12 19:32:39 -0400

    Template Matching main class

---
+++ TemplateMatching.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cursor_template = cv2.imread("C:/Users/roopa/Pictures/proj1_cse573/cursor_temp.jpg", 0)
+test_images = cv2.imread("C:/Users/roopa/Pictures/proj1_cse573/task3/pos_10.jpg", 0)
+
+
+w, h = cursor_template.shape[::-1]
+
+# Applying Gaussian blur and lapacian transform for the template
+cursor_blur = cv2.GaussianBlur(cursor_template, (3, 3), sigmaX=0)
+cursor_laplacian = cv2.Laplacian(cursor_blur, cv2.CV_8U)
+
+# Applying Gaussian blur and laplacian transform for the test images
+image_gaussian_blur = cv2.GaussianBlur(test_images, (5, 5), sigmaX=0)
+image_laplacian = cv2.Laplacian(image_gaussian_blur, cv2.CV_8U)
+
+matching_cursor = cv2.matchTemplate(image_laplacian, cursor_laplacian, cv2.TM_CCOEFF_NORMED)
+
+location = np.where(matching_cursor >= 0.67)
+
+
+for cursor in zip(*location[::-1]):
+    test_images = cv2.rectangle(test_images, cursor, (cursor[0] + w, cursor[1] + h), (255, 0, 0), 2)
+
+
+cv2.namedWindow('new_window', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('new_window', 720, 600)
+cv2.imshow('new_window', test_images)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
